71a473efd6ac11b8b3230bb2f5c926442880049c
Johannes Manurung <jmanurung753@gmail.com>
2022-01-18 15:06:36 +0700

    new

---
+++ app/src/main/java/com/doinglab/sdk/example/foodlenstestapplication/ListFood.java
@@ -0,0 +1,15 @@
+package com.doinglab.sdk.example.foodlenstestapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListFood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_food);
+        getSupportActionBar().hide();
+
+    }
+}
